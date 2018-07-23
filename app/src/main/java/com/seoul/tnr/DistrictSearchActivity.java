54bc7bea4375ee1278f7481b0e065213d210017d
charlie.9867 <charlie.9867@navercorp.com>
2018-07-23 19:04:12 +0900

    2018-07-23 charlie9867 메인 리스트 및 JSON 완료

--- app/src/main/java/com/seoul/tnr/DistrictSearchActivity.java
+++ app/src/main/java/com/seoul/tnr/DistrictSearchActivity.java
@@ -1,25 +1,27 @@
 package com.seoul.tnr;
 
 import android.app.Activity;
-import android.content.ClipData;
+import android.app.ProgressDialog;
 import android.content.Context;
 
 import android.content.Intent;
-import android.os.Handler;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 
 import com.seoul.cms.base.BaseActivity;
 import com.seoul.cms.base.BaseResponse;
-import com.seoul.cms.helper.LogHelper;
+import com.seoul.cms.helper.LoadingDialogHelper;
 import com.seoul.cms.model.AddressInfo;
 import com.seoul.cms.model.AddressList;
 
@@ -27,7 +29,14 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -41,11 +50,11 @@ public class DistrictSearchActivity extends BaseActivity {
     Activity activity = this;
 
     //이미지 배열 선언
-    ArrayList<String> imgArr = new ArrayList<String>();
+    ArrayList<String> districtArr = new ArrayList<String>();
 
-    private ArrayList<AddressInfo> mItems = new ArrayList<>();
+    String myJSON;
 
-
+    private static final String TAG_NAME = "juso";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,13 +63,18 @@ public class DistrictSearchActivity extends BaseActivity {
         super.setActionBarVisible(true);
         ButterKnife.bind(this);
 
-        String url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPZ5N0jd105TlA2z3oYXfzUn55HVlTDjqM9gMY0TRuBVvvgGxQ";
+/*        String url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPZ5N0jd105TlA2z3oYXfzUn55HVlTDjqM9gMY0TRuBVvvgGxQ";
         for (int i = 0 ; i < 10 ; i++) {
-            imgArr.add(Integer.toString(i));
+            districtArr.add(Integer.toString(i));
         }
-        GridAdapter gridAdapter = new GridAdapter(getApplicationContext(), imgArr);
+        GridAdapter gridAdapter = new GridAdapter(getApplicationContext(), districtArr);
 
-        districtListGv.setAdapter(gridAdapter);
+        districtListGv.setAdapter(gridAdapter);*/
+
+
+        GetData task = new GetData();
+
+        task.execute();
     }
 
     public void getAddressList() {
@@ -78,30 +92,131 @@ public class DistrictSearchActivity extends BaseActivity {
         });
     }
 
+    private class GetData extends AsyncTask<String, Void, String>{
+        String errorString = null;
 
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            LoadingDialogHelper.getInstance(DistrictSearchActivity.this).show();
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            LoadingDialogHelper.getInstance(DistrictSearchActivity.this).hide();
+            if (result != null) {
+                myJSON = result;
+                showResult();
+            }
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            //String searchKeyword = params[0];
+            String serverURL = "http://106.10.45.41";
+            serverURL = "http://49.236.136.62/address/district";
+            //String postParameters = "country=" + searchKeyword;
+            try {
+                URL url = new URL(serverURL);
+
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.setReadTimeout(5000);
+                httpURLConnection.setConnectTimeout(5000);
+                httpURLConnection.setRequestMethod("GET");
+                httpURLConnection.setDoInput(true);
+                httpURLConnection.connect();
+
+                /*OutputStream outputStream = httpURLConnection.getOutputStream();
+                //outputStream.write(postParameters.getBytes("UTF-8"));
+
+                outputStream.flush();
+                outputStream.close();*/
+
+                int responseStatusCode = httpURLConnection.getResponseCode();
+                Log.d("rescode", "response code - " + responseStatusCode);
+                InputStream inputStream;
+                if(responseStatusCode == HttpURLConnection.HTTP_OK) {
+                    inputStream = httpURLConnection.getInputStream();
+                }
+                else{
+                    inputStream = httpURLConnection.getErrorStream();
+                }
+
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                StringBuilder sb = new StringBuilder();
+                String line;
+
+                while((line = bufferedReader.readLine()) != null){
+                    sb.append(line);
+                }
+                Log.d("StringBuilder", "string - " + sb.toString());
+                bufferedReader.close();
+
+                return sb.toString().trim();
+
+            } catch (Exception e) {
+                errorString = e.toString();
+                Log.d("Exception Error", errorString);
+                return null;
+            }
+        }
+    }
+
+    private void showResult(){
+
+        try {
+
+            JSONObject jsonObject = new JSONObject(myJSON);
+
+            JSONArray jsonArray = jsonObject.getJSONArray("district_list");
+
+            for(int i=0;i<jsonArray.length();i++){
+
+                JSONObject item = jsonArray.getJSONObject(i);
+                String id = item.getString(TAG_NAME);
+
+                districtArr.add(id);
+                Log.d(TAG_NAME, id);
+                HashMap<String,String> hashMap = new HashMap<>();
+
+                hashMap.put(TAG_NAME, id);
+            }
+
+            GridAdapter gridAdapter = new GridAdapter(getApplicationContext(), districtArr);
+            districtListGv.setAdapter(gridAdapter);
+
+
+        } catch (JSONException e) {
+            Log.d("JSONException", "showResult : ", e);
+        }
+    }
 
 
     public class GridAdapter extends BaseAdapter {
         Context context;
         LayoutInflater inflater;
-        ArrayList<String> imgArr;
+        ArrayList<String> districtArr;
 
-        public GridAdapter(Context context, ArrayList<String> imgArr){
+        public GridAdapter(Context context, ArrayList<String> districtArr){
             this.context = context;
-            this.imgArr = imgArr;
+            this.districtArr = districtArr;
             inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         }
 
         @Override
         public int getCount() {
             // TODO Auto-generated method stub
-            return imgArr.size();
+            return districtArr.size();
         }
 
         @Override
         public Object getItem(int position) {
             // TODO Auto-generated method stub
-            return imgArr.get(position);
+            return districtArr.get(position);
         }
 
         @Override
@@ -118,14 +233,22 @@ public class DistrictSearchActivity extends BaseActivity {
                 convertView = inflater.inflate(R.layout.district_list_item, parent, false);
             }
 
-
             TextView districtTv = convertView.findViewById(R.id.districtTv);
+            LinearLayout listItemLl = convertView.findViewById(R.id.listItemLl);
+            LinearLayout.LayoutParams layoutParams = (LinearLayout.LayoutParams) listItemLl.getLayoutParams();
 
-            districtTv.setText(imgArr.get(position));
+            districtTv.setText(districtArr.get(position));
+
+            if (position%2==0){
+                layoutParams.leftMargin = 20;
+            } else {
+                layoutParams.rightMargin = 20;
+            }
+            listItemLl.setLayoutParams(layoutParams);
 
             /*ImageView imageView = convertView.findViewById(R.id.districtIv);
 
-            Glide.with(context).load(imgArr.get(position)).into(imageView);
+            Glide.with(context).load(districtArr.get(position)).into(imageView);
 
 
             imageView.setOnClickListener(new View.OnClickListener() {
@@ -141,8 +264,8 @@ public class DistrictSearchActivity extends BaseActivity {
                 @Override
                 public void onClick(View view) {
                     Intent intent = new Intent(activity, StreetSearchActivity.class);
+                    intent.putExtra("DISTRICT", districtArr.get(position));
                     startActivity(intent);
-
                 }
             });
             return convertView;
--- app/src/main/java/com/seoul/tnr/StreetSearchActivity.java
+++ app/src/main/java/com/seoul/tnr/StreetSearchActivity.java
@@ -3,17 +3,35 @@ package com.seoul.tnr;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.seoul.cms.base.BaseActivity;
+import com.seoul.cms.helper.LoadingDialogHelper;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -29,7 +47,13 @@ public class StreetSearchActivity extends BaseActivity {
     Activity activity = this;
 
     //이미지 배열 선언
-    ArrayList<String> imgArr = new ArrayList<String>();
+    ArrayList<String> streetArr = new ArrayList<String>();
+
+
+    String myJSON, district;
+
+
+    private static final String TAG_STREET = "dong";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,39 +61,147 @@ public class StreetSearchActivity extends BaseActivity {
         setContentView(R.layout.activity_street_search);
         ButterKnife.bind(this);
         super.setActionBarVisible(true);
-
+        Intent intent = getIntent();
+        district = intent.getExtras().getString("DISTRICT");
+        if (district.equals("")){
+            Toast.makeText(activity, "정보를 가져오지 못했습니다.", Toast.LENGTH_SHORT).show();
+            activity.finish();
+        }
         searchMenuTv.setText(R.string.searchMenuStreetEx);
 
-        for (int i = 0 ; i < 10 ; i++) {
-            imgArr.add(Integer.toString(i));
-        }
-        GridAdapter gridAdapter = new GridAdapter(getApplicationContext(), imgArr);
+        GetData task = new GetData();
 
-        streetListGv.setAdapter(gridAdapter);
+        task.execute();
+    }
+
+
+    private class GetData extends AsyncTask<String, Void, String> {
+        String errorString = null;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            LoadingDialogHelper.getInstance(StreetSearchActivity.this).show();
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            LoadingDialogHelper.getInstance(StreetSearchActivity.this).hide();
+
+            if (result != null) {
+                myJSON = result;
+                showResult();
+            }
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            //String searchKeyword = params[0];
+            Log.d("district", district);
+            String serverURL = "http://49.236.136.62/address/" + URLEncoder.encode(district);
+
+            //String postParameters = "country=" + searchKeyword;
+            try {
+                URL url = new URL(serverURL);
+
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.setReadTimeout(5000);
+                httpURLConnection.setConnectTimeout(5000);
+                httpURLConnection.setRequestMethod("GET");
+                httpURLConnection.setDoInput(true);
+                httpURLConnection.connect();
+
+                /*OutputStream outputStream = httpURLConnection.getOutputStream();
+                //outputStream.write(postParameters.getBytes("UTF-8"));
+
+                outputStream.flush();
+                outputStream.close();*/
+
+                int responseStatusCode = httpURLConnection.getResponseCode();
+                Log.d("rescode", "response code - " + responseStatusCode);
+                InputStream inputStream;
+                if(responseStatusCode == HttpURLConnection.HTTP_OK) {
+                    inputStream = httpURLConnection.getInputStream();
+                }
+                else{
+                    inputStream = httpURLConnection.getErrorStream();
+                }
+
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                StringBuilder sb = new StringBuilder();
+                String line;
+
+                while((line = bufferedReader.readLine()) != null){
+                    sb.append(line);
+                }
+                Log.d("StringBuilder", "string - " + sb.toString());
+                bufferedReader.close();
+
+                return sb.toString().trim();
+
+            } catch (Exception e) {
+                errorString = e.toString();
+                Log.d("Exception Error", errorString);
+                return null;
+            }
+        }
+    }
+
+    private void showResult(){
+
+        try {
+
+            JSONObject jsonObject = new JSONObject(myJSON);
+
+            JSONArray jsonArray = jsonObject.getJSONArray("street_list");
+
+            for(int i=0;i<jsonArray.length();i++){
+
+                JSONObject item = jsonArray.getJSONObject(i);
+                String id = item.getString(TAG_STREET);
+
+                streetArr.add(id);
+                Log.d(TAG_STREET, id);
+                HashMap<String,String> hashMap = new HashMap<>();
+
+                hashMap.put(TAG_STREET, id);
+            }
+
+            StreetSearchActivity.GridAdapter gridAdapter = new StreetSearchActivity.GridAdapter(getApplicationContext(), streetArr);
+            streetListGv.setAdapter(gridAdapter);
+
+
+        } catch (JSONException e) {
+            Log.d("JSONException", "showResult : ", e);
+        }
     }
 
 
     public class GridAdapter extends BaseAdapter {
         Context context;
         LayoutInflater inflater;
-        ArrayList<String> imgArr;
+        ArrayList<String> streetArr;
 
-        public GridAdapter(Context context, ArrayList<String> imgArr){
+        public GridAdapter(Context context, ArrayList<String> streetArr){
             this.context = context;
-            this.imgArr = imgArr;
+            this.streetArr = streetArr;
             inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         }
 
         @Override
         public int getCount() {
             // TODO Auto-generated method stub
-            return imgArr.size();
+            return streetArr.size();
         }
 
         @Override
         public Object getItem(int position) {
             // TODO Auto-generated method stub
-            return imgArr.get(position);
+            return streetArr.get(position);
         }
 
         @Override
@@ -88,12 +220,21 @@ public class StreetSearchActivity extends BaseActivity {
 
 
             TextView districtTv = convertView.findViewById(R.id.districtTv);
+            LinearLayout listItemLl = convertView.findViewById(R.id.listItemLl);
+            LinearLayout.LayoutParams layoutParams = (LinearLayout.LayoutParams) listItemLl.getLayoutParams();
 
-            districtTv.setText(imgArr.get(position));
+            districtTv.setText(streetArr.get(position));
+
+            if (position%3==0){
+                layoutParams.leftMargin = 20;
+            } else if (position%3 == 2) {
+                layoutParams.rightMargin = 20;
+            }
+            listItemLl.setLayoutParams(layoutParams);
 
             /*ImageView imageView = convertView.findViewById(R.id.districtIv);
 
-            Glide.with(context).load(imgArr.get(position)).into(imageView);
+            Glide.with(context).load(streetArr.get(position)).into(imageView);
 
 
             imageView.setOnClickListener(new View.OnClickListener() {
@@ -108,9 +249,10 @@ public class StreetSearchActivity extends BaseActivity {
             convertView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    Intent intent = new Intent(activity, StreetSearchActivity.class);
+                    Intent intent = new Intent(activity, ImageAddActivity.class);
+                    intent.putExtra("DISTRICT", district);
+                    intent.putExtra("STREET", streetArr.get(position));
                     startActivity(intent);
-
                 }
             });
             return convertView;
